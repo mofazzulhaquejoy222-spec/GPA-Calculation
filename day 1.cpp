050e737be7a2982918a29490a4d4e39e883a11d4
Joy <mofazzulhaquejoy222@gmail.com>
2026-08-02 17:53:19 +0600

    Update GPA.cpp

--- day 1.cpp
+++ day 1.cpp
@@ -20,16 +20,10 @@ int main ()
    cout<<"total credit number"<<sum<<endl;
    double r,sum_a=0,g[10];
    for(i=1;i<=n;i++){
-    //for(j=1;j==i;j++){
-        //for(a=1;a<=n;a++){
         g[i]=grade_point[i]*credit[i];
-        //}
         cout<<g[i]<<endl;
         sum_a=sum_a+g[i];
-    //}
-    //sum_a=sum_a+g[a];
    }
-   //sum_a=sum_a+g[i];
    cout<<"sum of grade and credit : "<<sum_a<<endl;
    r=sum_a/sum;
    cout<<"your GPA is : "<<r<<endl;
